69686bb7f8ea40da9bb2556048fe1138c73c62a2
rhaversen <rhaversen@gmail.com>
2024-04-23 07:18:45 +0200

    Linting

--- src/app/service/readyz/route.ts
+++ src/app/service/readyz/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
 export function GET() {
-    return new NextResponse('OK');
+	return new NextResponse('OK')
 }
