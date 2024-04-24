e153e466170b50ce2b0fb48d9ebbdb589aa60307
rhaversen <rhaversen@gmail.com>
2024-04-24 14:06:23 +0200

    Add order page components

---
+++ src/components/order/Products.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Product from "@/components/order/Product";
+
+type ProductProps = {
+	_id: string;
+	name: string;
+	description: string;
+	price: number;
+};
+
+type ProductsProps = {
+	products: ProductProps[];
+	quantities: Record<string, number>;
+	onQuantityChange: (key: string, newQuantity: number) => void;
+};
+
+const Products: React.FC<ProductsProps> = ({
+	products,
+	quantities,
+	onQuantityChange,
+}) => {
+	return (
+		<div>
+			{products.map((product) => (
+				<Product
+					key={product._id}
+					id={product._id}
+					initialQuantity={quantities[product._id]}
+					name={product.name}
+					description={product.description}
+					price={product.price}
+					onQuantityChange={onQuantityChange}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Products;
---
+++ src/components/order/SubmitButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type SubmitButtonProps = {
+	onClick: () => void;
+};
+
+const SubmitButton: React.FC<SubmitButtonProps> = ({ onClick }) => {
+	return (
+		<button
+			type="submit"
+			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			onClick={onClick}
+		>
+			Færdiggør Bestilling
+		</button>
+	);
+};
+
+export default SubmitButton;
