269ec05168846739492fcb3e737cf144ae98d1c1
rhaversen <rhaversen@gmail.com>
2024-05-04 23:19:39 +0200

    Handle product and options selection and option conditional rendering

--- src/components/orderstation/select/SelectionWindow.tsx
+++ src/components/orderstation/select/SelectionWindow.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ProductType, OptionType } from '@/app/orderstation/[room]/page'
 import ProductCatalog from '@/components/orderstation/select/ProductCatalog'
+import OptionsWindow from '@/components/orderstation/select/OptionsWindow'
 
 const SelectionWindow = ({
 	products,
@@ -11,12 +12,37 @@ const SelectionWindow = ({
 	options: OptionType[]
 	handleCartChange: (_id: string, type: 'products' | 'options', change: number) => void
 }) => {
+	const [showOptions, setShowOptions] = useState(false)
+	const [selectedProductOptions, setSelectedProductOptions] = useState<OptionType[]>([])
+
+	const handleProductSelect = (product: ProductType) => {
+		handleCartChange(product._id, 'products', 1)
+
+		if (product.options.length > 0) {
+			const productOptions = options.filter(option => product.options.includes(option._id))
+			setSelectedProductOptions(productOptions)
+			setShowOptions(true)
+		}
+	}
+
+	const handleOptionSelect = (option: OptionType) => {
+		handleCartChange(option._id, 'options', 1)
+		setShowOptions(false)
+	}
+
 	return (
 		<div>
 			<ProductCatalog
 				products={products}
-				onProductSelect={() => { }}
+				onProductSelect={handleProductSelect}
 			/>
+			{showOptions && (
+				<OptionsWindow
+					productOptions={selectedProductOptions}
+					onOptionSelect={handleOptionSelect}
+					onClose={() => { setShowOptions(false) }}
+				/>
+			)}
 		</div>
 	)
 }
