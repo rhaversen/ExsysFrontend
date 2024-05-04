3d20eff2b1b9ff152bb8ca5fe16f0f42a6926b07
rhaversen <rhaversen@gmail.com>
2024-05-04 21:40:45 +0200

    Add option component

---
+++ src/components/orderstation/select/Option.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+import { OptionType, ProductType } from '@/app/orderstation/[room]/page'
+
+const Option = ({
+	option,
+	onOptionSelect,
+}: {
+	option: OptionType
+	onOptionSelect: (_id: string) => void
+}) => {
+	return (
+		<div className='p-2 m-2 relative'>
+			<button
+				type='button'
+				className="cursor-pointer"
+				onClick={() => onOptionSelect(option?._id)}
+				draggable="false"
+			>
+				<div className="flex flex-row items-center justify-center">
+					<h3 className={'font-bold pr-2 text-black'}>{option.name}</h3>
+					<p className={'italic text-gray-700'}>{`${option.price === 0 ? '+' : ''}` + option.price + ' kr'}</p>
+				</div>
+				<Image
+					width={200}
+					height={200}
+					src={`${option.imageURL === undefined || option.imageURL === '' ? '/none.svg' : option.imageURL}`}
+					alt={option.name}
+					className="w-48 h-48 object-cover text-black"
+					draggable="false"
+					priority // Load image immediately
+				/>
+			</button>
+		</div>
+	)
+}
+
+export default Option
