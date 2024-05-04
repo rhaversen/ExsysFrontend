8e7f5c94bfaabe5577d43cdc7dc4309beed9f294
rhaversen <rhaversen@gmail.com>
2024-05-04 21:40:00 +0200

    images.domains is deprecated, using images.remotePatterns instead

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["via.placeholder.com"],
+		remotePatterns: [ {protocol: "https", hostname: "via.placeholder.com"}]
 	},
 };
 
