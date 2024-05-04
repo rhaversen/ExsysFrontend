4e90b993a82ee948f1100f0d91d5d260f560f79d
rhaversen <rhaversen@gmail.com>
2024-05-04 19:33:31 +0200

    Add room selector

---
+++ src/app/orderstation/layout.tsx
@@ -0,0 +1,19 @@
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Ny Skivehus Kantine',
+		default: 'Vælg Rum'
+	},
+	alternates: {
+		canonical: 'https://www.kantine.nyskivehus.dk/orderstation'
+	}
+}
+
+export default function SelectRoomLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return <section>{children}</section>
+}
---
+++ src/app/orderstation/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import Room from '@/components/orderstation/Room'
+
+export default function Page() {
+	const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+	const [rooms, setRooms] = useState<{ _id: string; name: string; description: string }[]>([])
+
+	const router = useRouter()
+
+	useEffect(() => {
+		if (API_URL === undefined || API_URL === null || API_URL === '') return
+
+		const fetchRooms = async () => {
+			try {
+				const response = await axios.get<{ _id: string; name: string; description: string; }[]>(API_URL + '/v1/rooms')
+				setRooms(response.data)
+			} catch (error) {
+				console.error('Failed to fetch rooms:', error)
+			}
+		}
+
+		fetchRooms()
+	}, [API_URL])
+
+	const handleRoomSelect = (roomId: string) => {
+		router.push(`/orderstation/${roomId}`)
+	}
+
+	return (
+		<main className='flex flex-col justify-center items-center h-screen'>
+			<h1 className='m-10 p-0 text-center text-black text-4xl'>Vælg et rum</h1>
+			<div className='flex flex-wrap justify-center items-center p-20'>
+				{rooms.map((room) => (
+					<Room
+						key={room._id}
+						room={room}
+						onRoomSelect={handleRoomSelect}
+					/>
+				))}
+			</div>
+		</main>
+	)
+}
