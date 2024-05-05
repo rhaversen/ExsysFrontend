a712b4729945168cbca1a9a90cc1297b3decf1b2
rhaversen <rhaversen@gmail.com>
2024-05-05 22:17:54 +0200

    Add type button to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,13 @@ export default function Page (): ReactElement {
 			<h1 className="mb-4 text-2xl font-bold text-gray-700">Vælg Opgave</h1>
 			<div className="flex space-x-4">
 				<button
+					type='button'
 					className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
 					onClick={() => { router.push('/orderstation') }}>
 					Bestillings Station
 				</button>
 				<button
+					type='button'
 					className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
 					onClick={() => { router.push('/admin') }}>
 					Personale
