1cdc983d1a4700068e602ccd39478431765e5445
rhaversen <rhaversen@gmail.com>
2024-05-05 17:34:26 +0200

    Add cart window

--- src/components/orderstation/cart/CartWindow.tsx
+++ src/components/orderstation/cart/CartWindow.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ProductType, OptionType } from '@/app/orderstation/[room]/page'
 import { CartType } from '@/app/orderstation/[room]/page'
+import OrderSummary from '@/components/orderstation/cart/OrderSummary'
+import SubmitButton from '@/components/ui/SubmitButton'
 
 const CartWindow = ({
 	products,
@@ -19,6 +21,22 @@ const CartWindow = ({
 }) => {
 	return (
 		<div>
+			<OrderSummary
+				products={products}
+				options={options}
+				cart={cart}
+				onCartChange={onCartChange}
+			/>
+			<div className="h-2 text-black">
+				{`Samlet Pris: ${(
+					Object.entries(cart.products).reduce((acc, [_id, quantity]) => acc + products.find(product => product._id === _id)?.price! * quantity, 0) +
+					Object.entries(cart.options).reduce((acc, [_id, quantity]) => acc + options.find(option => option._id === _id)?.price! * quantity, 0)
+				)} kr`}
+			</div>
+			<SubmitButton
+				disabled={!formIsValid}
+				onClick={onSubmit}
+			/>
 		</div>
 	)
 }
