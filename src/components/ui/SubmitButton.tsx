e2e5a18011de41bcafaf6eceadd95e165d946a0d
rhaversen <rhaversen@gmail.com>
2024-05-07 02:16:32 +0200

    Add constant width for submit button

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -13,7 +13,7 @@ const SubmitButton = ({
 		<div className="flex justify-center p-5">
 			<button
 				type="submit"
-				className={`font-bold py-5 px-10 rounded text-white
+				className={`w-60 font-bold py-5 px-10 rounded text-white
 				${disabled
 			? 'bg-blue-400 hover:bg-blue-400 cursor-not-allowed'
 			: 'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 cursor-pointer'}
