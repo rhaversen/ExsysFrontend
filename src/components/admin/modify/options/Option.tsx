9830e88ba83ee4bbf0b75c6fadd1a7bb2e5a8c9c
rhaversen <rhaversen@gmail.com>
2024-05-09 18:02:10 +0200

    Add editable option

---
+++ src/components/admin/modify/options/Option.tsx
@@ -0,0 +1,195 @@
+import { type OptionType } from '@/lib/backendDataTypes'
+import React, { type ReactElement, useState } from 'react'
+import Image from 'next/image'
+import EditableField from '@/components/admin/modify/ui/EditableField'
+import EditableImage from '@/components/admin/modify/ui/EditableImage'
+import ConfirmDeletion from '@/components/admin/modify/ui/ConfirmDeletion'
+import axios from 'axios'
+
+const Option = ({
+	option
+}: {
+	option: OptionType
+}): ReactElement => {
+	const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+	const [isEditing, setIsEditing] = useState(false)
+	const [newOption, setNewOption] = useState<OptionType>(option)
+	const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+
+	const patchOption = (option: OptionType, optionPatch: Omit<OptionType, '_id'>): void => {
+		axios.patch(`${API_URL}/v1/options/${option._id}`, optionPatch).then((response) => {
+			console.log('Option updated:', response.data)
+		}).catch((error) => {
+			console.error('Error updating option:', error)
+		})
+	}
+
+	const deleteOption = (option: OptionType, confirmDeletion: boolean): void => {
+		axios.delete(`${API_URL}/v1/options/${option._id}`, {
+			data: { confirmDeletion }
+		}).then(() => {
+			console.log('Option deleted')
+		}).catch((error) => {
+			console.error('Error deleting option:', error)
+		})
+	}
+
+	const handleNameChange = (v: string): void => {
+		console.log('Name change:', v)
+		setNewOption({ ...newOption, name: v })
+	}
+
+	const handlePriceChange = (v: string): void => {
+		v = v.replace(/[^0-9.]/g, '')
+		console.log('Price change:', v)
+		setNewOption({ ...newOption, price: Number(v) })
+	}
+
+	const handleImageChange = (v: string): void => {
+		console.log('Image change:', v)
+		setNewOption({ ...newOption, imageURL: v })
+	}
+
+	const handleUndoEdit = (): void => {
+		console.log('Undoing edit')
+		setNewOption(option)
+		setIsEditing(false)
+	}
+
+	const handleCompleteEdit = (): void => {
+		console.log('Completing edit')
+		patchOption(option, newOption)
+		setNewOption(newOption)
+		setIsEditing(false)
+	}
+
+	return (
+		<div className="p-2 m-2">
+			<div className="flex flex-col items-center justify-center">
+				<div className="flex flex-row items-center justify-center">
+					<div className="font-bold p-2 text-black">
+						<EditableField
+							text={newOption.name}
+							italic={false}
+							editable={isEditing}
+							edited={newOption.name !== option.name}
+							onChange={(v: string) => {
+								handleNameChange(v)
+							}}
+						/>
+					</div>
+					<div className="flex flex-row italic items-center text-gray-700">
+						<EditableField
+							text={newOption.price.toString()}
+							italic={true}
+							editable={isEditing}
+							edited={newOption.price !== option.price}
+							onChange={(v: string) => {
+								handlePriceChange(v)
+							}}
+						/>
+						<div className="pl-1">
+							{' kr'}
+						</div>
+					</div>
+				</div>
+				<EditableImage
+					defaultURL={option.imageURL}
+					newURL={newOption.imageURL}
+					editable={isEditing}
+					edited={newOption.imageURL !== option.imageURL}
+					onChange={(v: string) => {
+						handleImageChange(v)
+					}}
+				/>
+				{isEditing &&
+					<div className="flex flex-row">
+						<button
+							onClick={() => {
+								setShowDeleteConfirmation(true)
+							}}
+							type='button'
+							className="w-5 h-5"
+
+						>
+							<span className="sr-only">
+								{'Delete'}
+							</span>
+							<Image
+								width={20}
+								height={20}
+								className="w-full h-full"
+								src="/admin/modify/delete.svg"
+								alt="Delete"
+							/>
+						</button>
+						<button
+							onClick={handleUndoEdit}
+							type='button'
+							className="w-10 h-10"
+						>
+							<span className="sr-only">
+								{'Undo changes'}
+							</span>
+							<Image
+								width={40}
+								height={40}
+								className="w-full h-full"
+								src="/admin/modify/trashcan.svg"
+								alt="Undo"
+							/>
+						</button>
+						<button
+							onClick={handleCompleteEdit}
+							type='button'
+							className="w-10 h-10"
+						>
+							<span className="sr-only">
+								{'Complete changes'}
+							</span>
+							<Image
+								width={40}
+								height={40}
+								className="w-full h-full"
+								src="/admin/modify/checkmark.svg"
+								alt="Accept"
+							/>
+						</button>
+					</div>
+				}
+				{!isEditing &&
+					<button
+						onClick={() => {
+							setIsEditing(true)
+						}}
+						type='button'
+						className="w-10 h-10"
+					>
+						<Image
+							width={40}
+							height={40}
+							className="w-full h-full"
+							src="/admin/modify/pen.svg"
+							alt="Edit" />
+						<span className="sr-only">Edit</span>
+					</button>
+				}
+				{showDeleteConfirmation &&
+					<ConfirmDeletion
+						itemName={option.name}
+						onClose={() => {
+							setShowDeleteConfirmation(false)
+						}}
+						onSubmit={(confirm: boolean) => {
+							setShowDeleteConfirmation(false)
+							deleteOption(option, confirm)
+						}}
+					/>
+				}
+			</div>
+		</div>
+	)
+}
+
+export default Option
