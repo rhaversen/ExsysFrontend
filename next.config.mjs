ee0699b59557cb769234b5d5624ef51899367d9e
rhaversen <rhaversen@gmail.com>
2024-05-09 17:55:24 +0200

    Linting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [ {protocol: "https", hostname: "via.placeholder.com"}]
+		remotePatterns: [
+			{ protocol: "https", hostname: "via.placeholder.com" },
+		],
 	},
 };
 
