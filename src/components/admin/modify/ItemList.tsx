9ae2f256276d076c861e25bb36ae5dd443759393
rhaversen <rhaversen@gmail.com>
2024-05-10 02:51:20 +0200

    Linting

--- src/components/admin/modify/ItemList.tsx
+++ src/components/admin/modify/ItemList.tsx
@@ -21,7 +21,7 @@ const ItemList = ({
 			</ul>
 			<div className="bottom-0 flex justify-center">
 				<button
-					type='button'
+					type="button"
 					className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
 					onClick={onAdd}
 				>
