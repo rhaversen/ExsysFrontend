491f9e7e43a3ed5ddcac3aa6637dfec95a7d2757
rhaversen <rhaversen@gmail.com>
2024-05-10 02:52:22 +0200

    Item list is now a row

--- src/components/admin/modify/ItemList.tsx
+++ src/components/admin/modify/ItemList.tsx
@@ -12,21 +12,21 @@ const ItemList = ({
 	children: ReactNode[]
 }): ReactElement => {
 	return (
-		<div className="flex flex-col h-full">
-			<h2 className="text-2xl font-bold p-4 shadow-md text-center text-black">
-				{header}
-			</h2>
-			<ul className="no-scrollbar overflow-y-auto flex-1">
-				{children}
-			</ul>
-			<div className="bottom-0 flex justify-center">
+		<div className="flex flex-row w-screen pr-10 my-auto">
+			<div className="flex flex-col m-5">
+				<h2 className="text-2xl min-w-40 font-bold p-4 text-center text-black">
+					{header}
+				</h2>
 				<button
 					type="button"
-					className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+					className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
 					onClick={onAdd}
 				>
 					{buttonText}
 				</button>
+			</div>
+			<div className="no-scrollbar shadow-inner flex flex-row overflow-x-auto flex-1 bg-gray-300 rounded-xl">
+				{children}
 			</div>
 		</div>
 	)
