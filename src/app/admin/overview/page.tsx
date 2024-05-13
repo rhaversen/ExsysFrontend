f92698a31bdb3f876a2dffee92187f279a26b306
rhaversen <rhaversen@gmail.com>
2024-05-14 00:49:36 +0200

    Spacing

--- src/app/admin/overview/page.tsx
+++ src/app/admin/overview/page.tsx
@@ -25,6 +25,7 @@ export default function Page (): ReactElement {
 
 		const response = await axios.get(`${API_URL}/v1/orders?fromDate=${fromDate.toISOString()}&toDate=${toDate.toISOString()}&status=pending,confirmed`)
 		const data = response.data as OrderType[]
+
 		setOrders(data)
 	}, [API_URL])
 
