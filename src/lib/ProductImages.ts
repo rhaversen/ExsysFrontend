bd10db06333bd559937a61dccc2c830bd7e83f01
rhaversen <rhaversen@gmail.com>
2024-05-14 01:36:03 +0200

    Add product images

--- src/lib/ProductImages.ts
+++ src/lib/ProductImages.ts
@@ -1,1 +1,8 @@
-export const ProductImages = [] as string[]
+export const ProductImages = [
+    "cake.webp",
+    "cheese.webp",
+    "coffee.webp",
+    "spansk_rundstykke.webp",
+    "strawberry_jam.webp",
+    "tea.webp"
+] as string[]
