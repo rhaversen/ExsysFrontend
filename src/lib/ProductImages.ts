78e1f2793b5bf6d7904c6811e9d12f5df0014876
rhaversen <rhaversen@gmail.com>
2024-05-14 02:14:49 +0200

    Linting

--- src/lib/ProductImages.ts
+++ src/lib/ProductImages.ts
@@ -1,8 +1,8 @@
 export const ProductImages = [
-    "/productImages/cake.webp",
-    "/productImages/cheese.webp",
-    "/productImages/coffee.webp",
-    "/productImages/spansk_rundstykke.webp",
-    "/productImages/strawberry_jam.webp",
-    "/productImages/tea.webp"
+	'/productImages/cake.webp',
+	'/productImages/cheese.webp',
+	'/productImages/coffee.webp',
+	'/productImages/spansk_rundstykke.webp',
+	'/productImages/strawberry_jam.webp',
+	'/productImages/tea.webp'
 ] as string[]
