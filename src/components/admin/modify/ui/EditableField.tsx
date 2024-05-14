4f5a2ab7f7b4dbe10d798602716adcdc5ec361f9
rhaversen <rhaversen@gmail.com>
2024-05-14 04:21:56 +0200

    Set length to 1 when empty

--- src/components/admin/modify/ui/EditableField.tsx
+++ src/components/admin/modify/ui/EditableField.tsx
@@ -30,7 +30,7 @@ const EditableField = ({
 					onBlur={handleInputChange}
 					className={`${italic ? 'italic' : ''} text-center bg-transparent border-2 rounded-md cursor-text px-1 py-0.5 pr-1.5 transition-colors duration-200 ease-in-out focus:outline-none w-auto ${edited ? 'border-orange-400 hover:border-orange-600 focus:border-orange-700' : 'border-blue-500 hover:border-blue-600 focus:border-blue-700'}`}
 					readOnly={!editable}
-					size={text.length}
+					size={Math.max(text.length, 1)}
 					aria-label={text}
 				/>
 			}
