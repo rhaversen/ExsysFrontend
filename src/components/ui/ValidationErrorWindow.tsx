4d611ee7f56c7e1adf3b451eb054a3cc919f636a
rhaversen <rhaversen@gmail.com>
2024-05-16 04:34:09 +0200

    Always non italic

--- src/components/ui/ValidationErrorWindow.tsx
+++ src/components/ui/ValidationErrorWindow.tsx
@@ -6,7 +6,7 @@ const ValidationErrorWindow = ({
 	message: string
 }): ReactElement => {
 	return (
-		<div className='relative transform translate-x-2 translate-y-2'>
+		<div className='relative transform translate-x-2 translate-y-2 not-italic'>
 			<div className='absolute w-2 h-2 bg-red-800 transform rotate-45 -translate-y-1 translate-x-1'></div>
 			<div className='absolute rounded-md px-2 bg-red-800 text-white font-bold whitespace-nowrap overflow-ellipsis overflow-hidden'>
 				{message}
