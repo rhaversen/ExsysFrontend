392086a2fe807aa4362a4f80c07fd1be6927cd88
Rasmus Haversen <rhaversen@gmail.com>
2024-08-21 13:47:54 +0200

    Only push if authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 'use client'
 
+import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import React, { type ReactElement } from 'react'
 
 export default function Page (): ReactElement {
 	const router = useRouter()
+	const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+	const checkAuth = async (): Promise<boolean> => {
+		try {
+			await axios.get(`${API_URL}/v1/auth/is-authenticated`, { withCredentials: true })
+			return true
+		} catch (error: any) {
+			return false
+		}
+	}
 
 	return (
 		<main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -14,7 +25,13 @@ export default function Page (): ReactElement {
 					type="button"
 					className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
 					onClick={() => {
-						router.push('/orderstation')
+						checkAuth().then((isAuthenticated) => {
+							if (isAuthenticated) {
+								router.push('/orderstation')
+							} else {
+								router.push('/login-kiosk')
+							}
+						}).catch(console.error)
 					}}
 				>
 					Bestillings Station
@@ -23,7 +40,13 @@ export default function Page (): ReactElement {
 					type="button"
 					className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
 					onClick={() => {
-						router.push('/admin')
+						checkAuth().then((isAuthenticated) => {
+							if (isAuthenticated) {
+								router.push('/admin')
+							} else {
+								router.push('/login-admin')
+							}
+						}).catch(console.error)
 					}}
 				>
 					Personale
