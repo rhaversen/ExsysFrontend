a81d90d8325a944d575b566221051eb8400d1a67
Rasmus Haversen <rhaversen@gmail.com>
2024-08-31 00:01:32 +0200

    Add images for payment selection

--- src/components/orderstation/cart/SelectPaymentWindow.tsx
+++ src/components/orderstation/cart/SelectPaymentWindow.tsx
@@ -1,4 +1,5 @@
 import React, { type ReactElement } from 'react'
+import AsyncImage from '@/components/ui/AsyncImage'
 
 const SelectPaymentWindow = ({
 	onSubmit
@@ -7,20 +8,46 @@ const SelectPaymentWindow = ({
 }): ReactElement => {
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-			<div className="bg-white rounded-xl shadow-lg p-8 m-4 max-w-sm max-h-full text-gray-800 overflow-auto">
+			<div className="bg-white rounded-xl shadow-lg p-8 m-4 text-gray-800">
 				<h2 className="text-lg font-semibold text-center mb-4">Vælg Betalingsmetode</h2>
 				<div className="space-x-4 flex justify-center">
 					<button
 						onClick={() => { onSubmit('Card') }}
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+						className="py-2 px-6 focus:outline-none rounded-xl border-dotted border-2 border-blue-500"
+						type='button'
 					>
-						Kortbetaling
+						<div className='text-2xl font-bold text-center text-gray-800'>
+							{'Kort'}
+						</div>
+						<AsyncImage
+							src="/orderStation/creditcard.svg"
+							alt="Kort"
+							className="w-48 h-48"
+							width={200}
+							height={200}
+							quality={100}
+							priority={true}
+							draggable={false}
+						/>
 					</button>
 					<button
 						onClick={() => { onSubmit('Cash') }}
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+						className="py-2 px-6 focus:outline-none rounded-xl border-dotted border-2 border-blue-500"
+						type='button'
 					>
-						Kontantbetaling
+						<div className='text-2xl font-bold text-center text-gray-800'>
+							{'Kontant'}
+						</div>
+						<AsyncImage
+							src="/orderStation/coins.svg"
+							alt="Kontant"
+							className="w-48 h-48"
+							width={200}
+							height={200}
+							quality={100}
+							priority={true}
+							draggable={false}
+						/>
 					</button>
 				</div>
 			</div>
