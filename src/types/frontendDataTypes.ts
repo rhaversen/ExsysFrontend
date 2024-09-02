e3802541cc344eec107ff20763f22eb5a525acd9
Rasmus Haversen <rhaversen@gmail.com>
2024-09-02 04:35:33 +0200

    Add types for sorting

--- src/types/frontendDataTypes.ts
+++ src/types/frontendDataTypes.ts
@@ -24,3 +24,13 @@ export interface Validation {
 	validate: (value: string) => boolean
 	message: string
 }
+
+export const sortConfig = {
+	Produkter: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }, { prop: 'price', name: 'Pris' }],
+	Tilvalg: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }, { prop: 'price', name: 'Pris' }],
+	Aktiviteter: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }],
+	Rum: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }, { prop: 'description', name: 'Beskrivelse' }],
+	Kiosker: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }, { prop: 'kioskTag', name: 'Kiosk Tag' }, { prop: 'readerId', name: 'Reader ID' }],
+	Kortlæsere: [{ prop: 'readerTag', name: 'Reader Tag' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }],
+	Admins: [{ prop: 'name', name: 'Navn' }, { prop: 'createdAt', name: 'Oprettet' }, { prop: 'updatedAt', name: 'Opdateret' }]
+}
