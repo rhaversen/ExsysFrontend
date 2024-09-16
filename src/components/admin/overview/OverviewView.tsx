aa8c46708bf4cfb589d980db7c90de688cb6abaf
Rasmus Haversen <rhaversen@gmail.com>
2024-09-16 23:00:43 +0200

    Refactor to move order fetching to child component

--- src/components/admin/overview/OverviewView.tsx
+++ src/components/admin/overview/OverviewView.tsx
@@ -9,28 +9,58 @@ import {
 	type RoomType
 } from '@/types/backendDataTypes'
 import { type OrderTypeWithNames } from '@/types/frontendDataTypes'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { type ReactElement, useCallback, useEffect, useState } from 'react'
+import { useInterval } from 'react-use'
 
 const OverviewView = ({
-	orders,
 	products,
 	options,
 	rooms,
 	activities,
-	isFetching,
-	onUpdatedOrders
 }: {
-	orders: OrderType[]
 	products: ProductType[]
 	options: OptionType[]
 	rooms: RoomType[]
 	activities: ActivityType[]
-	isFetching: boolean
-	onUpdatedOrders: (orders: OrderType[]) => void
 }): ReactElement => {
+	const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+	const [orders, setOrders] = useState<OrderType[]>([])
 	const [ordersWithNames, setOrdersWithNames] = useState<OrderTypeWithNames[]>([])
 	const [roomOrders, setRoomOrders] = useState<Record<RoomType['_id'], OrderTypeWithNames[]>>({})
+
+	const getOrders = useCallback(async () => {
+		const fromDate = new Date()
+		fromDate.setHours(0, 0, 0, 0)
+		const toDate = new Date()
+		toDate.setHours(24, 0, 0, 0)
+
+		try {
+			const response = await axios.get(`${API_URL}/v1/orders?fromDate=${fromDate.toISOString()}&toDate=${toDate.toISOString()}&status=pending,confirmed&paymentStatus=successful`, { withCredentials: true })
+			const data = response.data as OrderType[]
+			setOrders(data)
+		} catch (error: any) {
+		}
+	}, [API_URL])
+
+	const handleOrdersUpdate = useCallback((orders: OrderType[]) => {
+		// Only update the orders that were changed
+		setOrders((prevOrders) => {
+			const newOrders = [...prevOrders]
+			orders.forEach((newOrder) => {
+				const index = newOrders.findIndex((order) => order._id === newOrder._id)
+				if (index === -1) return
+				if (newOrder.status === 'delivered') {
+					newOrders.splice(index, 1)
+					return
+				}
+				newOrders[index] = newOrder
+			})
+			return newOrders
+		})
+	}, [])
 
 	const addNamesToOrders = useCallback(() => {
 		return orders.map((order) => ({
@@ -63,7 +93,7 @@ const OverviewView = ({
 			roomOrders[roomName].push(order)
 		})
 		return roomOrders
-	}, [ordersWithNames, rooms, activities]) // Include activities in the dependency array
+	}, [ordersWithNames, rooms, activities])
 
 	useEffect(() => {
 		setRoomOrders(groupOrdersByRoom())
@@ -73,14 +103,11 @@ const OverviewView = ({
 		setOrdersWithNames(addNamesToOrders())
 	}, [setOrdersWithNames, addNamesToOrders])
 
+	useInterval(getOrders, 1000 * 10) // Fetch orders every 10 seconds
+
 	return (
 		<div>
-			{isFetching &&
-				<div className="flex justify-center flex-row">
-					<p className="p-10 font-bold text-gray-800 text-2xl">{'Henter Order...'}</p>
-				</div>
-			}
-			{orders.length === 0 && !isFetching &&
+			{orders.length === 0 &&
 				<p className="flex justify-center p-10 font-bold text-gray-800 text-2xl">{'Ingen Ordrer '}&#128522;</p>
 			}
 			{orders.length === 0 &&
@@ -103,7 +130,7 @@ const OverviewView = ({
 						key={room._id}
 						room={room}
 						orders={roomOrders[room.name] ?? []}
-						onUpdatedOrders={onUpdatedOrders}
+						onUpdatedOrders={handleOrdersUpdate}
 					/>
 				))}
 				{roomOrders['no-room'] !== undefined && roomOrders['no-room'].length > 0 &&
@@ -117,7 +144,7 @@ const OverviewView = ({
 							updatedAt: ''
 						}}
 						orders={roomOrders['no-room'] ?? []}
-						onUpdatedOrders={onUpdatedOrders}
+						onUpdatedOrders={handleOrdersUpdate}
 					/>
 				}
 			</div>
