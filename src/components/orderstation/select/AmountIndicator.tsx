1ddbf20565eebcd5356ee437543f28dfdd01dd09
Rasmus Haversen <rhaversen@gmail.com>
2024-09-16 14:08:25 +0200

    Handle more than 99 selected

--- src/components/orderstation/select/AmountIndicator.tsx
+++ src/components/orderstation/select/AmountIndicator.tsx
@@ -7,9 +7,9 @@ const AmountIndicator = ({
 }): ReactElement => {
 	return (
 		<div
-			className="w-10 h-10 z-50 bg-blue-500 rounded-full border-2 border-blue-600 flex justify-center items-center">
+			className={`${amount > 99 ? 'w-16' : 'w-10'} h-10 z-50 bg-blue-500 rounded-full border-2 border-blue-600 flex justify-center items-center`}>
 			<h3 className="text-2xl text-white">
-				{amount}
+				{amount > 99 ? '99+' : amount}
 			</h3>
 		</div>
 	)
