f0c0b8bcd736f5ae406bc4904becbdff6f61581d
Rasmus Haversen <rhaversen@gmail.com>
2024-09-27 02:09:06 +0200

    Fix layout titles

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,10 @@ import AdminAuthProvider from '@/contexts/AdminAuthProvider'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Admin',
+	title: {
+		template: '%s | Ny Skivehus Kantine',
+		default: 'Admin'
+	},
 	alternates: {
 		canonical: 'https://www.kantine.nyskivehus.dk/admin'
 	}
