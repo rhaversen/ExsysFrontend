dde00bd4bc60d151145fb821f93986f1364841b3
Rasmus Haversen <rhaversen@gmail.com>
2024-09-27 04:58:41 +0200

    Use websockets to listen for kiosks, products and options

--- src/app/orderstation/page.tsx
+++ src/app/orderstation/page.tsx
@@ -3,24 +3,24 @@
 import ActivitySelection from '@/components/orderstation/ActivitySelection'
 import OrderView from '@/components/orderstation/OrderView'
 import { useError } from '@/contexts/ErrorContext/ErrorContext'
+import useEntitySocketListeners from '@/hooks/CudWebsocket'
 import { convertOrderWindowFromUTC } from '@/lib/timeUtils'
-import {
-	type ActivityType,
-	type KioskTypeNonPopulated,
-	type OptionType,
-	type ProductType
-} from '@/types/backendDataTypes'
+import { type ActivityType, type KioskType, type OptionType, type ProductType } from '@/types/backendDataTypes'
 import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import React, { type ReactElement, useCallback, useEffect, useState } from 'react'
-import { useInterval } from 'react-use'
+import { io, type Socket } from 'socket.io-client'
 
 export default function Page (): ReactElement {
 	const API_URL = process.env.NEXT_PUBLIC_API_URL
+	const WS_URL = process.env.NEXT_PUBLIC_WS_URL
+
 	const { addError } = useError()
+	const router = useRouter()
 
 	const [products, setProducts] = useState<ProductType[]>([])
 	const [options, setOptions] = useState<OptionType[]>([])
-	const [kiosk, setKiosk] = useState<KioskTypeNonPopulated | null>(null)
+	const [kiosk, setKiosk] = useState<KioskType | null>(null)
 	const [checkoutMethods, setCheckoutMethods] = useState({
 		sumUp: false,
 		cash: true,
@@ -29,91 +29,170 @@ export default function Page (): ReactElement {
 	const [selectedActivity, setSelectedActivity] = useState<ActivityType | null>(null)
 	const [activities, setActivities] = useState<ActivityType[]>([])
 
+	// WebSocket Connection
+	const [socket, setSocket] = useState<Socket | null>(null)
+
 	// Helper function to fetch data with error handling
 	const fetchData = useCallback(async (url: string, config = {}): Promise<any> => {
 		const response = await axios.get(url, { withCredentials: true, ...config })
 		return response.data
 	}, [])
 
-	// Fetch products and options
-	const loadProductsAndOptions = useCallback(async (): Promise<void> => {
-		if (API_URL === undefined || API_URL === null || API_URL === '') return
-
-		const [productsData, optionsData] = await Promise.all([
-			fetchData(`${API_URL}/v1/products`),
-			fetchData(`${API_URL}/v1/options`)
-		])
-
-		const processedProducts: ProductType[] = productsData.map((product: ProductType) => ({
-			...product,
-			orderWindow: convertOrderWindowFromUTC(product.orderWindow)
-		}))
-
-		setProducts(processedProducts)
-		setOptions(optionsData as OptionType[])
-	}, [API_URL, fetchData])
-
-	// Fetch kiosk information
-	const loadKioskInfo = useCallback(async (): Promise<void> => {
-		if (API_URL === undefined || API_URL === null || API_URL === '') return
-
-		const kioskData: KioskTypeNonPopulated = await fetchData(`${API_URL}/v1/kiosks/me`)
-		setKiosk(kioskData)
+	const updateCheckoutMethods = useCallback((kioskData: KioskType) => {
 		setCheckoutMethods(prev => ({
 			...prev,
 			sumUp: kioskData.readerId !== null && kioskData.readerId !== undefined
 		}))
-	}, [API_URL, fetchData])
+	}, [])
 
-	// Fetch activities and related kiosk activities
-	const loadActivities = useCallback(async (): Promise<void> => {
+	// Process product data
+	const processProductData = (product: ProductType): ProductType => ({
+		...product,
+		orderWindow: convertOrderWindowFromUTC(product.orderWindow)
+	})
+
+	// Process all products data
+	const processProductsData = useCallback((productsData: ProductType[]): ProductType[] => {
+		return productsData.map(processProductData)
+	}, [])
+
+	// Load all data
+	const loadData = useCallback(async (): Promise<void> => {
 		if (API_URL === undefined || API_URL === null || API_URL === '') return
 
-		const [kioskData, activitiesData]: [KioskTypeNonPopulated, ActivityType[]] = await Promise.all([
-			fetchData(`${API_URL}/v1/kiosks/me`),
-			fetchData(`${API_URL}/v1/activities`)
-		])
+		try {
+			const [
+				kioskData,
+				productsData,
+				optionsData,
+				activitiesData
+			]: [KioskType, ProductType[], OptionType[], ActivityType[]] = await Promise.all([
+				fetchData(`${API_URL}/v1/kiosks/me`),
+				fetchData(`${API_URL}/v1/products`),
+				fetchData(`${API_URL}/v1/options`),
+				fetchData(`${API_URL}/v1/activities`)
+			])
 
-		const kioskActivities = activitiesData.filter(activity =>
-			kioskData.activities.some(kioskActivity => kioskActivity._id === activity._id)
-		)
+			// Process and set data
+			setKiosk(kioskData)
+			setProducts(processProductsData(productsData))
+			setOptions(optionsData)
+			setActivities(activitiesData)
 
-		setActivities(kioskActivities)
+			// Update checkout methods based on kiosk data
+			updateCheckoutMethods(kioskData)
 
-		if (kioskActivities.length === 1) {
-			setSelectedActivity(kioskActivities[0])
+			// If only one activity is available, select it
+			if (kioskData.activities.length === 1) {
+				setSelectedActivity(kioskData.activities[0])
+			}
+		} catch (error) {
+			addError(error)
 		}
-	}, [API_URL, fetchData])
+	}, [API_URL, fetchData, updateCheckoutMethods, processProductsData, addError])
 
-	// Initial data fetching on component mount
+	// Initialize data on mount
+	useEffect(() => {
+		loadData().catch(addError)
+	}, [addError, loadData])
+
+	// Initialize WebSocket connection
 	useEffect(() => {
 		if (API_URL === undefined || API_URL === null || API_URL === '') return
+		const socketInstance = io(WS_URL)
+		setSocket(socketInstance)
 
-		Promise.all([
-			loadProductsAndOptions(),
-			loadKioskInfo(),
-			loadActivities()
-		]).catch(addError)
-	}, [API_URL, addError, loadActivities, loadKioskInfo, loadProductsAndOptions])
+		return () => {
+			socketInstance.disconnect()
+		}
+	}, [API_URL, WS_URL])
 
-	// Set up intervals to refetch data every hour
-	useInterval(loadProductsAndOptions, 1000 * 60 * 60) // Every hour
-	useInterval(loadActivities, 1000 * 60 * 60) // Every hour
-	useInterval(loadKioskInfo, 1000 * 60 * 60) // Every hour
+	// Products
+	useEntitySocketListeners<ProductType>(
+		socket,
+		'product',
+		item => { setProducts(prev => [...prev, processProductData(item)]) },
+		item => {
+			setProducts(prev => {
+				const updated = processProductData(item)
+				return prev.map(p => (p._id === updated._id ? updated : p))
+			})
+		},
+		id => { setProducts(prev => prev.filter(p => p._id !== id)) }
+	)
+
+	// Options
+	useEntitySocketListeners<OptionType>(
+		socket,
+		'option',
+		item => { setOptions(prev => [...prev, item]) },
+		item => { setOptions(prev => prev.map(o => (o._id === item._id ? item : o))) },
+		id => { setOptions(prev => prev.filter(o => o._id !== id)) }
+	)
+
+	// Activities
+	useEntitySocketListeners<ActivityType>(
+		socket,
+		'activity',
+		activity => { setActivities(prev => [...prev, activity]) },
+		activity => {
+			if (kiosk === null) return
+			// If the selected activity is no longer associated with the kiosk, deselect it
+			if (!kiosk.activities.some(a => a._id === activity._id)) {
+				setSelectedActivity(null)
+			}
+
+			// Update the activity in the activities list
+			setActivities(prev => prev.map(a => (a._id === activity._id ? activity : a)))
+		},
+		id => {
+			setActivities(prev => prev.filter(a => a._id !== id))
+			if (selectedActivity?._id === id) {
+				setSelectedActivity(null)
+			}
+		}
+	)
+
+	// Kiosk WebSocket Listeners
+	useEntitySocketListeners<KioskType>(
+		socket,
+		'kiosk',
+		() => { }, // No add handler for kiosks
+		kioskUpdate => {
+			if ((kiosk !== null) && kioskUpdate._id === kiosk._id) {
+				setKiosk(kioskUpdate)
+				updateCheckoutMethods(kioskUpdate)
+
+				// If the selected activity is no longer associated with the kiosk, deselect it
+				if (!kioskUpdate.activities.some(a => a._id === selectedActivity?._id)) {
+					setSelectedActivity(null)
+				}
+
+				// If only one activity is available, select it¨
+				if (kioskUpdate.activities.length === 1) {
+					setSelectedActivity(kioskUpdate.activities[0])
+				}
+			}
+		},
+		() => {
+			// If the kiosk is deleted, redirect to login
+			router.push('/login-kiosk')
+		}
+	)
 
 	return (
 		<div>
 			{selectedActivity === null && (
 				<ActivitySelection
-					activities={activities}
+					activities={activities.filter(activity => kiosk?.activities.some(a => a._id === activity._id)).sort((a, b) => a.name.localeCompare(b.name))}
 					onActivitySelect={setSelectedActivity}
 				/>
 			)}
 			{selectedActivity !== null && kiosk !== null && (
 				<OrderView
 					kiosk={kiosk}
-					products={products}
-					options={options}
+					products={products.sort((a, b) => a.name.localeCompare(b.name))}
+					options={options.sort((a, b) => a.name.localeCompare(b.name))}
 					activity={selectedActivity}
 					checkoutMethods={checkoutMethods}
 					onClose={() => { setSelectedActivity(null) }}
