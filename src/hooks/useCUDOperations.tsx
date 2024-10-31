4e8fb3d88e575097eb189f30e4e24efa5af25dcc
Rasmus Haversen <rhaversen@gmail.com>
2024-10-31 14:31:37 +0100

    Add types

--- src/hooks/useCUDOperations.tsx
+++ src/hooks/useCUDOperations.tsx
@@ -2,19 +2,19 @@ import { useCallback } from 'react'
 import axios from 'axios'
 import { useError } from '@/contexts/ErrorContext/ErrorContext'
 
-const useCUDOperations = <T,>(entityPath: string): {
-	createEntity: (data: any) => void
-	updateEntity: (id: string, data: any) => void
+const useCUDOperations = <PostType, PatchType>(entityPath: string): {
+	createEntity: (data: PostType) => void
+	updateEntity: (id: string, data: PatchType) => void
 	deleteEntity: (id: string, confirm: boolean) => void
 } => {
 	const API_URL = process.env.NEXT_PUBLIC_API_URL
 	const { addError } = useError()
 
-	const createEntity = useCallback((data: T) => {
+	const createEntity = useCallback((data: PostType) => {
 		axios.post(`${API_URL}${entityPath}`, data, { withCredentials: true }).catch(addError)
 	}, [API_URL, entityPath, addError])
 
-	const updateEntity = useCallback((id: string, data: Partial<T>) => {
+	const updateEntity = useCallback((id: string, data: PatchType) => {
 		axios.patch(`${API_URL}${entityPath}/${id}`, data, { withCredentials: true }).catch(addError)
 	}, [API_URL, entityPath, addError])
 
