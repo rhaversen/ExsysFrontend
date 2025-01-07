70bf289ca8a73d29099b4789c6b6fc984d7ec9d7
Rasmus Haversen <rhaversen@gmail.com>
2025-01-07 10:53:26 +0100

    Refactor ItemList component layout for improved button styling and alignment

--- src/components/admin/modify/ui/ItemList.tsx
+++ src/components/admin/modify/ui/ItemList.tsx
@@ -12,26 +12,22 @@ const ItemList = ({
 	children: ReactNode[]
 }): ReactElement => {
 	return (
-		<div>
-			<div className="flex justify-center">
-				<button
-					type="button"
-					title="Tilføj"
-					className="relative w-1/4 m-5 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded items-center justify-center"
-					onClick={onAdd}
-				>
-					<div className="absolute left-2 top-1/2 transform -translate-y-1/2 flex items-center">
-						<Image
-							className="h-7 w-7"
-							src={AdminImages.add.src}
-							alt={AdminImages.add.alt}
-							width={10}
-							height={10}
-						/>
-					</div>
-					<span className="block text-center w-full">{buttonText}</span>
-				</button>
-			</div>
+		<div className='flex flex-col items-center'>
+			<button
+				type="button"
+				title="Tilføj"
+				className="flex w-auto m-5 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded items-center justify-center"
+				onClick={onAdd}
+			>
+				<Image
+					className="h-7 w-7"
+					src={AdminImages.add.src}
+					alt={AdminImages.add.alt}
+					width={10}
+					height={10}
+				/>
+				<span className="p-2 mx-5 font-bold">{buttonText}</span>
+			</button>
 			<div className="flex flex-wrap justify-evenly">
 				{children}
 			</div>
