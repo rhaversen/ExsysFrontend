0c3973ff42367cb37f25b0cb9efc89699e67d93d
Rasmus Haversen <rhaversen@gmail.com>
2025-02-22 19:55:10 +0100

    No animation for chached images

--- src/components/ui/AsyncImage.tsx
+++ src/components/ui/AsyncImage.tsx
@@ -1,6 +1,6 @@
 import { LoadingImage } from '@/lib/images'
 import Image from 'next/image'
-import React, { type ReactElement, useState } from 'react'
+import React, { type ReactElement, useState, useEffect } from 'react'
 
 const AsyncImage = ({
 	className,
@@ -23,6 +23,18 @@ const AsyncImage = ({
 }): ReactElement => {
 	const [imageLoaded, setImageLoaded] = useState(false)
 	const [loadingLoaded, setLoadingLoaded] = useState(false)
+	const [skipAnimation, setSkipAnimation] = useState(false)
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (!imageLoaded) {
+				setSkipAnimation(false)
+			}
+		}, 50)
+
+		setSkipAnimation(true)
+		return () => { clearTimeout(timer) }
+	}, [])
 
 	return (
 		<div className={className}>
@@ -34,7 +46,7 @@ const AsyncImage = ({
 					src={LoadingImage.src}
 					alt={LoadingImage.alt}
 					priority
-					className={`h-full w-full transition-opacity duration-300 ease-in-out ${loadingLoaded ? 'opacity-100' : 'opacity-0'}`}
+					className={`h-full w-full ${skipAnimation ? '' : 'transition-opacity duration-100 ease-in-out'} ${loadingLoaded ? 'opacity-100' : 'opacity-0'}`}
 					draggable="false"
 					onLoad={() => {
 						setLoadingLoaded(true)
@@ -47,7 +59,7 @@ const AsyncImage = ({
 				quality={quality}
 				src={src}
 				alt={alt}
-				className={`transition-opacity duration-300 ease-in-out ${imageLoaded ? 'opacity-100 h-full w-full' : 'opacity-0 h-0 w-0'}`}
+				className={`${skipAnimation ? '' : 'transition-opacity duration-300 ease-in-out'} ${imageLoaded ? 'opacity-100 h-full w-full' : 'opacity-0 h-0 w-0'}`}
 				draggable={draggable}
 				priority={priority}
 				onLoad={() => {
