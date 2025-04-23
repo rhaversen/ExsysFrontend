ff0bc550da6b0eef2d47ab0ef73439db1a2b557c
rhaversen <rhaversen@gmail.com>
2025-04-23 03:15:49 +0200

    Rebuild nextjs app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Geist, Geist_Mono } from 'next/font/google'
 import { type ReactElement } from 'react'
 
 import DailyReloader from '@/components/DailyReloader'
@@ -8,7 +8,15 @@ import UserProvider from '@/contexts/UserProvider'
 
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin']
+})
 
 export const metadata: Metadata = {
 	title: {
@@ -27,11 +35,13 @@ export const metadata: Metadata = {
 export default function RootLayout ({
 	children
 }: Readonly<{
-	children: React.ReactNode
+	children: React.ReactNode;
 }>): ReactElement {
 	return (
-		<html lang="da">
-			<body className={inter.className}>
+		<html lang='da'>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
 				<ErrorProvider>
 					<UserProvider>
 						<DailyReloader />
