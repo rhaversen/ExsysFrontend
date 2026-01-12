f1ff4ce316cd5081a766c87f7b4ead0be152bcf9
rhaversen <rhaversen@gmail.com>
2026-01-12 17:58:11 +0100

    Linting

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-import type { NextConfig } from 'next'
 import { execSync } from 'child_process'
+
+import type { NextConfig } from 'next'
 
 const getGitHash = (): string => {
 	try {
