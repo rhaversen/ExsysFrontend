383aad5fe0f282114eb21ec6bc3f3b55a292ad12
Rasmus Haversen <rhaversen@gmail.com>
2026-02-22 14:28:31 +0100

    Reset selected room on activity selection

--- src/app/kiosk/page.tsx
+++ src/app/kiosk/page.tsx
@@ -108,6 +108,7 @@ export default function Page (): ReactElement {
 	const handleActivitySelect = useCallback((activity: typeof selectedActivity) => {
 		if (!activity) { return }
 		track('activity_select', { activityId: activity._id })
+		setSelectedRoom(null)
 		setSelectedActivity(activity)
 
 		const activityRooms = getRoomsForActivity(activity)
@@ -117,15 +118,10 @@ export default function Page (): ReactElement {
 			track('nav_auto_to_order')
 			navigateTo('order')
 		} else {
-			if (selectedRoom !== null) {
-				track('nav_auto_to_order')
-				navigateTo('order')
-			} else {
-				track('nav_auto_to_room')
-				navigateTo('room')
-			}
+			track('nav_auto_to_room')
+			navigateTo('room')
 		}
-	}, [selectedRoom, setSelectedActivity, setSelectedRoom, navigateTo, getRoomsForActivity, track])
+	}, [setSelectedActivity, setSelectedRoom, navigateTo, getRoomsForActivity, track])
 
 	const handleRoomSelect = useCallback((room: typeof selectedRoom) => {
 		if (!room) { return }
